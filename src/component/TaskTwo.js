58d93c06aa64cb8dd055b1edc989259adf359980
ShashiSahani <shashisahani496@gmail.com>
2023-08-16 13:27:14 +0530

    add functionality using filter method

--- src/component/TaskTwo.js
+++ src/component/TaskTwo.js
@@ -1,13 +1,32 @@
-import React, { useState } from 'react'
-import "../App.css"
+import React, { useState } from "react";
+import "../App.css";
 function TaskTwo() {
-    const [task,setTasks]=useState([{id:1,name:"Building Profile",completed:false},{},{}])
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "Building Profile", completed: false },
+    { id: 2, name: "Task building", completed: true },
+    { id: 3, name: "Tracking Profile", completed: false },
+  ]);
+
+  const handleDelete=(id)=>{
+    console.log(id)
+    setTasks(tasks.filter(item=>id !==item.id));
+   
+  }
   return (
     <div>
-        <p>Task List</p>
-      
+      <p>Task List</p>
+      <ul>
+    {tasks.map((item)=>(
+        
+            <li key={item.id}>
+                <span>{item.id} - {item.name}</span>
+                <button onClick={()=>handleDelete(item.id)} className="delete">Delete</button>
+            </li>
+     
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default TaskTwo
+export default TaskTwo;
